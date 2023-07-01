f8b037176f33e91da9ce60f439863b8dd30b496e
Gustavo <gustavomacedosousa013@gmail.com>
2023-07-01 18:49:42 -0300

    Criando a função de conhecer o usuário

--- robo.py
+++ robo.py
@@ -2,5 +2,9 @@ from datetime import datetime
 def apresentacao():
     nome = input("Qual o nome do robo?")
     idade = int(input("Qual a minha idade?"))
-    print(f"Olá {nome}, prazer em conhecê-lo!")
+    print(f"Olá! Me chamo {nome}")
     print(f"Eu fui criado em {datetime.now().year - idade}")
+
+def conhecendo_user():
+    usuario = input("Qual o seu nome usuário?")
+    print(f"Prazer em conhecê-lo {usuario}!")
